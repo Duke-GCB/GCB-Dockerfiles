0e8a744612445c9a29a2fc1ef715a1c924680687
Dan Leehr <dan.leehr@duke.edu>
2015-04-06 14:23:56 -0400

    First attempt at trimmomatic wrapper script, not yet run

--- trimmomatic/trimmomatic.sh
+++ trimmomatic/trimmomatic.sh
@@ -4,69 +4,100 @@ set -e
 
 # This is a script for a docker image to run trimmomatic, a java program
 # See http://www.usadellab.org/cms/?page=trimmomatic
-# It translates environment variables and wraps fastqc commands
-# Paired mode
 #
 # CONT prefix means this variable is expected to be set in the container at runtime
 
 # INPUTS
 #
-# CONT_INPUT_FASTQ_READ_1
-# CONT_INPUT_FASTQ_READ_2
-# CONT_INPUT_ADAPTERS_FILE
+# CONT_INPUT_FASTQ_READ_1 - Required for both Paired end and single end
+# CONT_INPUT_FASTQ_READ_2 - Required only for Paired end
+# CONT_INPUT_ADAPTERS_FILE - Required
 #
 # PARAMETERS
 #
+# CONT_PARAM_TRIM_MODE  PE or SE, required.
 # CONT_PARAM_QUALITY_SCORE  default -phred33
-# CONT_PARAM_ADAPTERS_PARAMS
-# CONT_PARAM_STEPS default  LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:30
+# CONT_PARAM_ILLUMINACLIP_SETTINGS - settings for <seed mismatches>:<palindrome clip threshold>:<simple clip threshold>
+# CONT_PARAM_ADDITIONAL_STEPS default LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:30
+#
 # OUTPUTS
 #
-# CONT_OUTPUT_PAIRED_1
-# CONT_OUTPUT_UNPAIRED_1
-# CONT_OUTPUT_PAIRED_2
-# CONT_OUTPUT_UNPAIRED_2
-# CONT_OUTPUT_ERROR_LOG
+# CONT_OUTPUT_SE_TRIMMED - Required for SE
+# CONT_OUTPUT_PE_TRIMMED_P1 - Required for PE
+# CONT_OUTPUT_PE_TRIMMED_P2 - Required for PE
+# CONT_OUTPUT_PE_TRIMMED_U1 - Required for PE
+# CONT_OUTPUT_PE_TRIMMED_U2 - Required for PE
 
-# PE is paired ends mode, used for RNA_SEQ
-# command from IMD
-# java -jar /data/reddylab/software/Trimmomatic-0.32/trimmomatic-0.32.jar PE \
-#   -threads 1 \                # threads
-#   -phred33 \                  # quality score - Phred-33
-#   RNA_seq_1.fastq \           # input1
-#   RNA_seq_2.fastq \           # input2
-#   RNA_seq_1.trimmed.P.fastq \ # paired output 1
-#   RNA_seq_1.trimmed.U.fastq \ # unpaired output 1
-#   RNA_seq_2.trimmed.P.fastq \ # paired output 2
-#   RNA_seq_2.trimmed.U.fastq \ # unpaired output 2
-#   adapters.fa:2:30:15 \       # Are we missing ILLUMINACLIP here?
-#   LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:30 \ # more steps
-#   2 > RNA.trim_err.txt # Redirect stderr
+# Check that trim mode is set
+[ -z "$CONT_PARAM_TRIM_MODE" ] && echo "Error: The CONT_PARAM_TRIM_MODE variable must be set" && exit 1
 
-# old code below here
+# Check parameters based on trim mode
+if [ "PE" -eq "$CONT_PARAM_TRIM_MODE" ]
+then
+  # If PE, need 2 inputs and 4 outputs
+  for VARNAME in CONT_INPUT_FASTQ_READ_1 CONT_INPUT_FASTQ_READ_2 CONT_INPUT_ADAPTERS_FILE do
+    eval INFILE=\$$VARNAME
+    # Check that input variables are set
+    [ -z "$INFILE" ] && echo "Error: The $VARNAME variable must be set" && exit 1
+    # Check that input files are readable
+    [ ! -r "$INFILE" ] && echo "Error: input file $INFILE does not exist or cannot be read" && exit 1
+  done
+  for VARNAME in CONT_OUTPUT_PE_TRIMMED_P1 CONT_OUTPUT_PE_TRIMMED_P2 CONT_OUTPUT_PE_TRIMMED_U1 CONT_OUTPUT_PE_TRIMMED_U2 do
+    eval OUTFILE=\$$VARNAME
+    # Check that output variables are set
+    [ -z "$OUTFILE" ] && echo "Error: The $VARNAME variable must be set" && exit 1
+    # Check that output directories are writable
+    [ ! -w $(dirname "$OUTFILE" ) ] && echo "Error: output file $OUTFILE is not writable" && exit 1
+  done
+  TRIMMOMATIC_BIN=`which TrimmomaticPE`
+  $INPUTS="$CONT_INPUT_FASTQ_READ_1 $CONT_INPUT_FASTQ_READ_2"
+  $OUTPUTS="$CONT_OUTPUT_PE_TRIMMED_P1 $CONT_OUTPUT_PE_TRIMMED_P2 $CONT_OUTPUT_PE_TRIMMED_U1 $CONT_OUTPUT_PE_TRIMMED_U2"
+elif [ "SE" -eq "$CONT_PARAM_TRIM_MODE" ]
+then
+  # If SE, one input and 1 output
+  for VARNAME in CONT_INPUT_FASTQ_READ_1 CONT_INPUT_ADAPTERS_FILE do
+    eval INFILE=\$$VARNAME
+    # Check that input variables are set
+    [ -z "$INFILE" ] && echo "Error: The $VARNAME variable must be set" && exit 1
+    # Check that input files are readable
+    [ ! -r "$INFILE" ] && echo "Error: input file $INFILE does not exist or cannot be read" && exit 1
+  done
+  # Check that output variables are set
+  [ -z "$CONT_OUTPUT_SE_TRIMMED" ] && echo "Error: The CONT_OUTPUT_SE_TRIMMED variable must be set" && exit 1
+  # Check that output directories are writable
+  [ ! -w $(dirname "$CONT_OUTPUT_SE_TRIMMED" ) ] && echo "Error: output file $CONT_OUTPUT_SE_TRIMMED is not writable" && exit 1
+  TRIMMOMATIC_BIN=`which TrimmomaticSE`
+  $INPUTS=$CONT_INPUT_FASTQ_READ_1
+  $OUTPUTS=$CONT_OUTPUT_SE_TRIMMED
+else
+  echo "Error: Invalid CONT_PARAM_TRIM_MODE: $CONT_PARAM_TRIM_MODE" && exit 1
+fi
 
-# Check that variables are set
-[ -z "$CONT_INPUT_FASTQ_FILE" ] && echo "Error: The CONT_INPUT_FASTQ_FILE variable must be set" && exit 1
+# Mode, input and output set. Check other parameters
+# If non-default values provided to container, use them.
+# ILLUMINACLIP_STEP includes the adapters file
+ILLUMINACLIP_STEP="ILLUMINACLIP:$CONT_INPUT_ADAPTERS_FILE:2:30:15"
+ADDITIONAL_STEPS="LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:30"
+THREADS="-threads 1"
+QUALITY_SCORE="-phred33"
 
-# Check that output file does not exist
-# These break if there are spaces
-[ -e "$CONT_OUTPUT_FASTQC_REPORT" ] && echo "Error: output file at $CONT_OUTPUT_FASTQC_REPORT already exists" && exit 1
+if [ ! -z "$CONT_PARAM_ILLUMINACLIP_SETTINGS" ]; then
+  ILLUMINACLIP_STEP="ILLUMINACLIP:$CONT_INPUT_ADAPTERS_FILE:$CONT_PARAM_ILLUMINACLIP_SETTINGS"
+fi
 
-# Check that output file is writable
-[ ! -w $(dirname "$CONT_OUTPUT_FASTQC_REPORT") ] && echo "Error: output file $CONT_OUTPUT_FASTQC_REPORT is not writable" && exit 1
+if [ ! -z "$CONT_PARAM_ADDITIONAL_STEPS" ]; then
+  ADDITIONAL_STEPS=$CONT_PARAM_ADDITIONAL_STEPS
+fi
 
-FASTQC_BIN=$(which fastqc)
-FASTQC_VERSION=$($FASTQC_BIN --version)
-WORKDIR=$(mktemp -d)
-FASTQC_CMD="$FASTQC_BIN -o $WORKDIR $CONT_INPUT_FASTQ_FILE --noextract"
+if [ ! -z "$CONT_PARAM_QUALITY_SCORE" ]; then
+  $QUALITY_SCORE=$CONT_PARAM_QUALITY_SCORE
+fi
+
+TRIMMOMATIC_CMD="$TRIMMOMATIC_BIN $THREADS $QUALITY_SCORE $INPUTS $OUTPUTS $ILLUMINACLIP_STEP $ADDITIONAL_STEPS"
+TRIMMOMATIC_VERSION=$(readlink "/usr/share/java/trimmomatic.jar")
 
 echo
 echo "Starting $0..."
-echo "$FASTQC_VERSION"
-echo "$FASTQC_CMD"
-sh -c "$FASTQC_CMD"
-
-# Output files are created in workdir, and are named basename of input file + _fastqc.zip
-INPUT_FILE_NAME=$(basename $CONT_INPUT_FASTQ_FILE)
-ZIPFILE=$WORKDIR/${INPUT_FILE_NAME%.*}_fastqc.zip
-mv $ZIPFILE $CONT_OUTPUT_FASTQC_REPORT
+echo "$TRIMMOMATIC_VERSION"
+echo "$TRIMMOMATIC_CMD"
+sh -c "$TRIMMOMATIC_CMD"
