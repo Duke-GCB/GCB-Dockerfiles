a9b034891ff160e806e45af8ac7752cfe8b4f665
John Bradley <johnbradley2008@gmail.com>
2018-02-02 16:04:11 -0500

    changes to run tests with singularity
    Approach taken from https://github.com/singularityware/singularity/issues/429#issuecomment-269089311
    1. Creates docker container using local image.
    2. Create singularity image
    3. export docker image | import new singularity image

--- test.py
+++ test.py
@@ -30,10 +30,31 @@ def run_docker_get_output(imagename, cmd):
      return str(exc.output)
 
 
+def run_singularity_get_output(imagename, cmd):
+  run_bash_cmd("docker create --name for_export {}".format(imagename))
+  run_bash_cmd("sudo singularity image.create imported.img")
+  run_bash_cmd("docker export for_export | sudo singularity image.import imported.img")
+  run_bash_cmd("docker rm for_export")
+  docker_cmd = "singularity exec imported.img {} 2>&1".format(cmd)
+  try:
+     print("RUN", docker_cmd)
+     ret = str(subprocess.check_output(["bash", "-c", docker_cmd]))
+     os.remove("imported.img")
+     return ret
+  except subprocess.CalledProcessError as exc:
+     ret = str(exc.output)
+     os.remove("imported.img")
+     return ret
+
+
+def run_bash_cmd(command):
+  return str(subprocess.check_output(["bash", "-c", command]))
+
+
 def run_tests(imagename, filename):
   had_error = False
   for cmd, expect_text in get_test_list(filename):
-     expect_pattern = re.compile(expect_text)
+     expect_pattern = re.compile(expect_text, re.MULTILINE)
      print("running {} inside {}".format(cmd, imagename))
      docker_output = run_docker_get_output(imagename, cmd)
      if not re.match(expect_pattern, docker_output):
@@ -41,6 +62,13 @@ def run_tests(imagename, filename):
         print("Expected Regex: {}".format(expect_text))
         print("Actual: {}".format(docker_output))
         had_error = True
+     sing_output = run_singularity_get_output(imagename, cmd)
+     if not re.match(expect_pattern, sing_output):
+        print("Error running {}".format(cmd))
+        print("Expected Regex: {}".format(expect_text))
+        print("Actual: {}".format(sing_output))
+        had_error = True
+
   return had_error
 
 
@@ -50,7 +78,7 @@ def find_unittest_info(dir_name):
     for name in files:
       if name == "unittest.yml":
         test_directory = root
-        image_name = "dukegcb/{}".format(test_directory.replace("./", "").replace("/", ":"))
+        image_name = "test_{}".format(test_directory.replace("./", "").replace("/", ":"))
         test_filename = os.path.join(root, name)
         test_info.append((test_directory, image_name, test_filename))
   return test_info
