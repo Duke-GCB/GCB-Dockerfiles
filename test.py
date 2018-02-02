d8be95963b7bf3c10400689bde215543b8ad89a6
John Bradley <johnbradley2008@gmail.com>
2018-02-02 13:12:49 -0500

    script to test images using docker
    Looks through all directories for unittest.yml files.
    It assumes there is a Dockerfile in the directory with this file.
    It guesses a name for the image.
    Builds the Dockerfile tagging that name using docker.
    For each command, expected_output pair in the unittest.yml file:
       run the image passing the command
       compares the expected_output(regex) against the output from the image
       exits if any images failed
    
    WIP

---
+++ test.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import subprocess
+import yaml
+import re
+
+
+def get_test_list(filename):
+  with open(filename) as infile:
+    testinfo_list = []
+    unittest_config = yaml.load(infile)
+    for testinfo in unittest_config['commands']:
+      cmd = testinfo['cmd']
+      expect_text = testinfo['expect_text']
+      testinfo_list.append((cmd, expect_text))
+    return testinfo_list
+
+
+def build_docker_image(imagename, build_path):
+  print("Building docker image for path {}".format(build_path))
+  docker_cmd = "docker build -t {} {}".format(imagename, build_path)
+  return str(subprocess.check_output(["bash", "-c", docker_cmd]))
+
+
+def run_docker_get_output(imagename, cmd):
+  docker_cmd = "docker run -it {} {}".format(imagename, cmd)
+  try:
+     return str(subprocess.check_output(["bash", "-c", docker_cmd]))
+  except subprocess.CalledProcessError as exc:
+     return str(exc.output)
+
+
+def run_tests(imagename, filename):
+  had_error = False
+  for cmd, expect_text in get_test_list(filename):
+     expect_pattern = re.compile(expect_text)
+     print("running {} inside {}".format(cmd, imagename))
+     docker_output = run_docker_get_output(imagename, cmd)
+     if not re.match(expect_pattern, docker_output):
+        print("Error running {}".format(cmd))
+        print("Expected Regex: {}".format(expect_text))
+        print("Actual: {}".format(docker_output))
+        had_error = True
+  return had_error
+
+
+def find_unittest_info(dir_name):
+  test_info = []
+  for root, dirs, files in os.walk("."):
+    for name in files:
+      if name == "unittest.yml":
+        test_directory = root
+        image_name = "dukegcb/{}".format(test_directory.replace("./", "").replace("/", ":"))
+        test_filename = os.path.join(root, name)
+        test_info.append((test_directory, image_name, test_filename))
+  return test_info
+
+
+if __name__ == "__main__":
+  base_directory = "."
+  for test_directory, image_name, test_filename in find_unittest_info(base_directory):
+     print(test_directory, image_name, test_filename)
+
+  build_docker_image(image_name, test_directory)
+  had_error = run_tests(image_name, test_filename)
+  if had_error:
+     sys.exit(1)
