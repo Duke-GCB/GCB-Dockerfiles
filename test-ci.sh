faf1d31088f4a24c64f58b0411e2858f274cb705
John Bradley <johnbradley2008@gmail.com>
2018-02-12 16:17:11 -0500

    update test-ci.sh with functions.sh improvements
    Should now test Dockerfile/unittest.yml changes across all commits in the current branch.

--- test-ci.sh
+++ test-ci.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# CI Testing Script for GCB-Dockerfiles
+#
+# Runs tests for changed Dockerfile/unittest.yml file
+
 set -e
 source functions.sh
-sha=$(last_nonmerge_commit_sha)
-paths=$(changed_paths_in_commit $sha)
+# See build-ci.sh for explanation of these conventions/rules
+check_org
+check_deploy_branch
+compare_range=$(get_compare_range)
+paths=$(changed_paths_in_range "$compare_range")
 python3 tests/imagecheck.py "$DOCKERHUB_ORG" $paths
